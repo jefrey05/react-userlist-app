dda3589afff917086f7f663e1919b8053eef4347
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-08-08 18:58:11 -0400

    added error modal to app

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,8 +1,9 @@
-import React,{useState} from "react";
+import React,{Fragment, useState} from "react";
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
 import Button from '../UI/Button';
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from '../Helpers/Wrapper';
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
@@ -45,7 +46,7 @@ const AddUser = (props) => {
     setError(null);
    }
   return (
-    <div> 
+    <Fragment> 
       {error&& <ErrorModal title = {error.title} message = {error.message} onConfirm={errorHandler}/>}
     <Card className = {classes.input}>
       <form onSubmit={addUserHandler}>
@@ -57,7 +58,7 @@ const AddUser = (props) => {
         <Button type="submit">Add User</Button>
       </form>
     </Card>
-    </div>
+    </Fragment>
   );
 };
 
