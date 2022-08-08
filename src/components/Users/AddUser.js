5033dc8d13b1184f3244cba2d7cca80f2657d144
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-08-08 18:35:42 -0400

    added error modal to app

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -40,9 +40,13 @@ const AddUser = (props) => {
   const ageChangeHandler  = event =>{
     setEnteredAge(event.target.value);
    }
+
+   const errorHandler = ()=>{
+    setError(null);
+   }
   return (
-    <div>
-      {error&& <ErrorModal title = {error.title} message = {error.message}/>}
+    <div> 
+      {error&& <ErrorModal title = {error.title} message = {error.message} onConfirm={errorHandler}/>}
     <Card className = {classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
